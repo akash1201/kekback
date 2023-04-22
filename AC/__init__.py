d08186a26c2d8658bd68f94a076f04a53d8686b4
Bharadwaj Ramachandran <zbram@Bharadwajs-MacBook-Pro-2.local>
2023-04-22 15:59:02 -0700

    dbname

--- AC/__init__.py
+++ AC/__init__.py
@@ -17,7 +17,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'KekronMekron-Hask-Key'
 
     # init db 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:letmein123@ac-dev.cl2yr0yt9ms9.us-west-1.rds.amazonaws.com:5432/ac-dev'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:letmein123@ac-dev.cl2yr0yt9ms9.us-west-1.rds.amazonaws.com:5432/postgres'
     db.init_app(app)
     migrate.init_app(app,db)
     
